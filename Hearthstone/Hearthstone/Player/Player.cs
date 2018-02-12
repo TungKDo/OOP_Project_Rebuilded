6715417dd0e9f1f6d7677937bcba086f2c303a70
Unknown <dokhanhtung1993@gmail.com>
2018-02-12 17:22:46 +0200

    Revert "11"
    This reverts commit da5e09f7ba4ac66c42fbca8e974ad7a6b8882041.

--- Hearthstone/Hearthstone/Player/Player.cs
+++ Hearthstone/Hearthstone/Player/Player.cs
@@ -11,23 +11,19 @@ namespace Hearthstone.Player
     class Player : IPlayer
     {
         private int maxManaCrystals;
-        private int healthPoints;
-        private bool isDead;
 
         public Player(Deck playerDeck)
         {
-            ManaCrystal = 0;
+            ManaCrystal = 0; //ok
             MaxManaCrystal = 0;
             this.PlayerDeck = playerDeck;
             PlayerHand = new List<ICard>();
             BattleField = new List<ICreature>();
             HealthPoints = 15;
-            IsDead = false;
         }
-        public int ManaCrystal { get; set; }
+        public int ManaCrystal { get ; set ; }
 
-        public int MaxManaCrystal
-        {
+        public int MaxManaCrystal {
             get
             {
                 return maxManaCrystals;
@@ -42,71 +38,39 @@ namespace Hearthstone.Player
 
         public List<ICard> PlayerHand { get; set; }
         public List<ICreature> BattleField { get; set; }
-        public int HealthPoints
-        {
-            get
-            {
-                return this.healthPoints;
-            }
-            set
-            {
-                this.healthPoints = value;
-                if(value <0)
-                {
-                    this.IsDead = true;
-                }
-            }
-        }
+        public int HealthPoints { get; set; }
 
-        public bool IsDead
+        public void AttackWithCreature(ICreature attacking IPlayer opponent)
         {
-            get
-            {
-                return this.isDead;
-            }
-            set
-            {
-                this.isDead = value;
-            }
-        }
-
-        public void AttackWithCreature(ICreature attackingCreature, IDamageable opponent)
-        {
-            attackingCreature.Attack(opponent);
+            
+            ICreature 
         }
 
         public ICreature SelectACreature(string creatureName)
         {
             creatureName = creatureName.ToLower();
-            ICreature myCreature = this.BattleField.FirstOrDefault(x => x.Name.ToLower() == creatureName);
+            ICreature myCreature = (ICreature)this.BattleField.FirstOrDefault(x => x.Name.ToLower() == creatureName);
             return myCreature;
         }
 
-        public ISpell SelectASpell(string spellName)
-        {
-            spellName = spellName.ToLower();
-            ISpell spell = (ISpell)this.PlayerHand.FirstOrDefault(x => x.Name.ToLower() == spellName);
-            return spell;
-        }
         public void DrawCard()
         {
-            ICard cardToBeDraw = PlayerDeck.Cards[0];
-            this.PlayerHand.Add(cardToBeDraw);
-            this.PlayerDeck.Cards.Remove(cardToBeDraw);
+            throw new NotImplementedException();
         }
 
-
-        public void PlayCreature(ICreature creature)
+        public bool IsDead()
         {
-            PlayerHand.Remove(creature);
-            BattleField.Add(creature);
+            throw new NotImplementedException();
         }
 
-        public void PlaySpell(ISpell spell,IDamageable opponent)
+        public void PlayCreature()
         {
-            opponent.HealthPoints = spell.Damage;
-            
+            throw new NotImplementedException();
         }
 
+        public void PlaySpell(IPlayer opponent)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
